a8f8a969a63b7970d7dac9669a860141f4b2353e
matiprg <kheirzadehsch@gmail.com>
2023-06-17 16:07:10 +0300

    make link urls for blog-post-categories

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from blog.views import *
-
-
+from blog.views import  *
 
 app_name='blog'
-
 urlpatterns = [
     path('',blog_view,name='home'),
     path('<int:pid>',blog_single,name='single'),
+    path('category/<str:cat_name>',blog_view,name='category'),
     #path('post-<int:pid>',test,name='test'),
+    path('test',test,name='test'),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,29 +2,39 @@ from django.utils import timezone
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 
-def blog_view(request):
+
+def blog_view(request,cat_name=None):
         posts=Post.objects.filter(status=1)
-        # زمان فعلی
-        current_time = timezone.now()  
-        # فیلتر کردن پست‌ها بر اساس زمان
-        posts = Post.objects.filter(published_date__lte=current_time)
+        if cat_name:
+                posts=posts.filter(category__name=cat_name)
+        #current_time = timezone.now()  
+        #posts = Post.objects.filter(published_date__lte=current_time)
         context={'posts': posts}
         return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
     posts=Post.objects.filter(status=1)
     post=get_object_or_404(posts,pk=pid)
+    previous_post=Post.objects.filter(pk__lt=post.pk).order_by('-pk').first()
+    next_post=Post.objects.filter(pk__gt=post.pk).order_by('pk').first()
     post.counted_views+=1
     post.save()
-    context={'post':post}
-    return render(request,'blog/blog-single.html',context)
+    #context={'post':post}
+    return render(request,'blog/blog-single.html',{'post':post,'previous_post':previous_post, 'next_post': next_post})
 
-def test(request,pid):
-    #posts=Post.objects.filter(status=1)
-    #post=Post.objects.get(id=pid)
-    post=get_object_or_404(Post,pk=pid)
-    context={'post':post}
-    return render(request,'test.html',context)
+def blog_category(request,cat_name):
+        posts=Post.objects.filter(status=1)
+        posts=posts.filter(category__name=cat_name)  
+        context={'posts': posts}
+        return render(request,'blog/blog-home.html',context) 
 
+def test(request):
+      return render(request,'test.html')
+
+    # posts=Post.objects.filter(status=1)
+    # post=Post.objects.get(id=pid)
+    # post=get_object_or_404(Post,pk=pid)
+    # context={'post':post}
+  
 
    
